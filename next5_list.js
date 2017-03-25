8a18f21ff123e33514f4656daf8c7cb5834a5958
MauriceKeigtley <maurice.keightley@gmail.com>
2017-03-25 11:47:24 +1000

    Dummy Data working
    CountDown timers working -
    Tables shows fake data

--- next5_list.js
+++ next5_list.js
@@ -1,28 +1,71 @@
-// Set the date we're counting down to
-var countDownDate = new Date("Mar 25, 2017 15:37:25").getTime();
+
+function getData() {
+    // The function returns the product of p1 and p2
+
+	var test_races = [
+		[1,"Tauranga", 1490409900],
+		[2,"Winton", 1490419900],
+		[3,"Auckland", 1490414800],
+		[4,"Tauranga", 1490404800],
+		[5,"Winton", 1490434800]
+		
+	];
+	return test_races;	
+}
+
+function setTimer(races){
+	for (var i = 0; i < races.length; i++) { 
+	
+		var raceId = "race"+races[i][0];
+		var meetingId = "meeting"+races[i][0];
+		
+		raceId = raceId.replace(/^\s+|\s+$/g,"");
+		
+		var countDownDate = new Date(races[i][2] * 1000);
+		//get todays date
+		var now = new Date().getTime();
+		
+		// Find the distance between now an the count down date
+		var distance = countDownDate - now;
+
+		// Time calculations for days, hours, minutes and seconds
+		var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+		var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+		var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+		var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+
+			// Display the result in the element with id="demo"
+		document.getElementById(meetingId).innerHTML = races[i][1];
+		document.getElementById(raceId).innerHTML = days + "d " + hours + "h "
+		+ minutes + "m " + seconds + "s ";
+	}
+	
+
+	return distance;
+
+	
+}
+
+
+
+var races = getData();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
+	//setup test array 
+	
+		distance = setTimer(races);
+		// If the count down is finished, write some text 
+		if (distance < 0) {
+			clearInterval(x);
+			document.getElementById("race1").innerHTML = "Closed";
+		}
+	
 
-  // Get todays date and time
-  var now = new Date().getTime();
 
-  // Find the distance between now an the count down date
-  var distance = countDownDate - now;
 
-  // Time calculations for days, hours, minutes and seconds
-  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+	
+	
 
-  // Display the result in the element with id="demo"
-  document.getElementById("race1").innerHTML = days + "d " + hours + "h "
-  + minutes + "m " + seconds + "s ";
-
-  // If the count down is finished, write some text 
-  if (distance < 0) {
-    clearInterval(x);
-    document.getElementById("race1").innerHTML = "EXPIRED";
-  }
 }, 1000);
